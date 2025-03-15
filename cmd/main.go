9c71a38c3e54bb4c595900dc2bccc3c0e0f21ac9
Vivek Rawat <vivek.rawat888@gmail.com>
2025-03-15 11:05:25 +0530

    removed testing code

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"yumyum-pi/Hardeol/core/routes"
 	"yumyum-pi/Hardeol/core/server"
 )
@@ -13,15 +11,6 @@ func main() {
 	r := routes.New()
 	srv := server.New(":8080", r)
 
-	r.Handle("/addRoute", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("addRoute called")
-		go r.Handle("/rounte2", func(w2 http.ResponseWriter, req2 *http.Request) {
-			w2.Write([]byte("route2"))
-		})
-
-		fmt.Println("addRoute write")
-		w.Write([]byte("Addeed route2"))
-	})
 	// Start serving. ListenAndServe will block.
 	log.Println("Starting server on :8080")
 	if err := srv.Serve(); err != nil {
